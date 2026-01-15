1b7202650e3a5cac82b97c51a06b497d41ea6a29
rhit-huangr2 <huangr2@rose-hulman.edu>
2026-01-14 19:18:31 -0500

    revert to previous commit if need be

---
+++ public/scripts/main.js
@@ -0,0 +1,86 @@
+var rhit = rhit || {};
+
+
+rhit.main = function () {
+    console.log("Ready");
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            const displayName = user.displayName;
+            const email = user.email;
+
+            const photoURL = user.photoURL;
+            const phoneNumber = user.phoneNumber;
+            const isAnonymous = user.isAnonymous;
+            const uid = user.uid;
+
+            console.log("the user is signed in: ", uid);
+            console.log(displayName);
+            console.log(email);
+            console.log(photoURL);
+            console.log(phoneNumber);
+            console.log(isAnonymous);
+        }
+        else {
+            console.log("no user signed in");
+        }
+    });
+
+    const inputEmailEl = document.querySelector("#inputEmail");
+    const inputPasswordEl = document.querySelector("#inputPassword");
+
+    document.querySelector("#signOutButton").onclick = (event) => {
+        console.log(`Sign out`);
+
+        firebase.auth().signOut().then(function () {
+            console.log("you are now signed out");
+        }).catch(function (error) {
+            console.log("sign out error");
+        });
+    };
+    document.querySelector("#createAccountButton").onclick = (event) => {
+        console.log(`Create account ${inputEmailEl.value} password: ${inputPasswordEl.value}`);
+        firebase.auth().createUserWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("Create account error ", errorCode, errorMessage);
+
+        });
+    };
+    document.querySelector("#logInButton").onclick = (event) => {
+        console.log(`Log in for email: ${inputEmailEl.value} password: ${inputPasswordEl.value}`);
+        firebase.auth().signInWithEmailAndPassword(inputEmailEl.value, inputPasswordEl.value).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("log in error", errorCode, errorMessage);
+
+        });
+    };
+
+    document.querySelector("#anonymousAuthButton").onclick = (event) => {
+
+        firebase.auth().signInAnonymously().catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("Anonymous auth error", errorCode, errorMessage);
+        })
+    };
+    rhit.startFirebaseUI();
+
+};
+rhit.startFirebaseUI = function () {
+    var uiConfig = {
+        signInSuccessUrl: '/',
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+        ],
+    };
+
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig);
+}
+
+rhit.main();
